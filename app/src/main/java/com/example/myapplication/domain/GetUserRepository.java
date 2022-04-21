445403703f0973616514b7f522920132935e5f12
cindy <cindy@cindys-MacBook-Pro.local>
2022-04-21 17:46:40 +0900

    commit

--- app/src/main/java/com/example/myapplication/domain/GetUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.domain;
-
-import com.example.myapplication.model.BeerModel;
-import java.util.List;
-import io.reactivex.Single;
-
-public interface GetUserRepository {
-    Single<List<BeerModel>> getUser();
-}
